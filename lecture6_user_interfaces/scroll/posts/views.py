84c3e942403cb5abbd120ae28f686dc3fcc98d14
Sraosha47 <sorusch.afkhami@gmail.com>
2023-03-18 23:16:46 +0100

    created an API that gives a json with requested posts

--- lecture6_user_interfaces/scroll/posts/views.py
+++ lecture6_user_interfaces/scroll/posts/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "posts/index.html")
+
+def posts(request):
+
+    #get start and end points
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or (start + 9))
+
+    #generate 'posts' for demo purposes
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    #delay response
+    time.sleep(1)
+
+    #return list of posts as JSON
+    return JsonResponse({
+        "posts": data
+    })
+#basically created an API
