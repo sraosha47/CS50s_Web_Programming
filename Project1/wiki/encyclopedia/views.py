45a15d9a4c4d849762b0143ab3798215610388da
Sraosha47 <sorusch.afkhami@gmail.com>
2023-03-23 19:16:42 +0100

    Added a wiki.html, links to the list of entries and tried to get them to lead to the correct entry

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -5,6 +5,10 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def wiki(request):
+    return render(request, "encyclopedia/wiki.html", {
+        "content": util.get_entry("CSS")
+        })
