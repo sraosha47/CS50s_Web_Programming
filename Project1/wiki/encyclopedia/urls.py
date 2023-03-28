b351648755b96b283cbcec59f3a23a32dc157460
Sraosha47 <sorusch.afkhami@gmail.com>
2023-03-28 14:25:53 +0200

    the path to the wiki.html is now simply the name of the entry

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/"+"TITLE", views.wiki, name="wiki")
+    path("<str:entry>", views.wiki, name="wiki")
 ]
